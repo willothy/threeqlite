7e8b0652c175e858431f2b13948b08ac51be64bf
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-11-27 22:04:37 -0800

    asyncify + lockifying

--- src/vfs.rs
+++ src/vfs.rs
@@ -9,18 +9,30 @@ struct Inner {
     s3: aws_sdk_s3::Client,
     bucket: String,
     lock_file: String,
-    current_lock: Arc<std::sync::Mutex<Option<Vec<u16>>>>,
+    current_lock: Arc<std::sync::Mutex<Option<Vec<u8>>>>,
 }
 
-impl Inner {
-    pub async fn request_lock(&self) -> Result<std::io::Error> {
+pub trait Lock {
+    async fn request_lock(&self) -> Result<Vec<u8>, std::io::Error>;
+}
+
+struct S3FileLock {
+    s3: aws_sdk_s3::Client,
+    bucket: String,
+    lock_file: String,
+    current_lock: std::sync::Mutex<Option<Vec<u8>>>,
+}
+
+
+impl Lock for S3FileLock {
+    async fn request_lock(&self) -> Result<Vec<u8>, std::io::Error> {
         let lock_uuid = uuid::Uuid::new_v4().to_bytes_le();
         loop {
             match self
                 .s3
                 .get_object_legal_hold()
-                .bucket(self.bucket)
-                .key(self.lock_file)
+                .bucket(self.bucket.clone())
+                .key(self.lock_file.clone())
                 .send()
                 .await
             {
@@ -31,9 +43,9 @@ impl Inner {
                                 match self
                                     .s3
                                     .put_object()
-                                    .bucket(self.bucket)
-                                    .key(self.lock_file)
-                                    .body(lock_uuid.to_vec())
+                                    .bucket(&self.bucket)
+                                    .key(&self.lock_file)
+                                    .body(lock_uuid.to_vec().into())
                                     .object_lock_legal_hold_status(ObjectLockLegalHoldStatus::On)
                                     .send()
                                     .await
@@ -56,8 +68,53 @@ impl Inner {
             }
         }
     }
+}
 
-    
+
+impl Inner {
+    pub async fn request_lock(&self) -> Result<Vec<u8>, std::io::Error> {
+        let lock_uuid = uuid::Uuid::new_v4().to_bytes_le();
+        loop {
+            match self
+                .s3
+                .get_object_legal_hold()
+                .bucket(self.bucket.clone())
+                .key(self.lock_file.clone())
+                .send()
+                .await
+            {
+                Ok(lock_status) => {
+                    if let Some(status) = lock_status.legal_hold {
+                        if let Some(status) = status.status {
+                            if status == ObjectLockLegalHoldStatus::Off {
+                                match self
+                                    .s3
+                                    .put_object()
+                                    .bucket(&self.bucket)
+                                    .key(&self.lock_file)
+                                    .body(lock_uuid.to_vec().into())
+                                    .object_lock_legal_hold_status(ObjectLockLegalHoldStatus::On)
+                                    .send()
+                                    .await
+                                {
+                                    Ok(_) => {
+                                        let vect = lock_uuid.to_vec();
+                                        *self.current_lock.lock().unwrap() = Some(vect.clone());
+                                        return Ok(vect);
+                                    }
+                                    Err(_) => {
+
+                                        // retry
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                Err(_) => {}
+            }
+        }
+    }
 }
 
 #[derive(Clone)]
